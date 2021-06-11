ad0d20b0b58d1b7c696cd141e3ae0d72b13cb53e
leo <74308238+leodin0717@users.noreply.github.com>
2021-06-11 09:15:16 +0900

    login

---
+++ 202106/0610 class js/login_form.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <script type="text/javascript">
+    function check_input() {
+      if (!document.login_form.id.value) {
+        alert("아이디를 입력하세요");
+        document.login_form.id.focus();
+        return;
+      }
+      if (!document.login_form.pass.value) {
+        alert("비밀번호를 입력하세요");
+        document.login_form.pass.focus();
+        return;
+      }
+      document.login_form.submit();
+    }
+  </script>
+</head>
+<body>
+  <header>
+    <?php include "header.php";?>
+  </header>
+  <hr>
+  <section>
+    <form name="login_form" method="post" action="login.php">
+      <p>
+        아이디 : <input type="text" name="id"><br>
+        비밀번호 : <input type="password" name="pass"><br>
+        <input type="button" value="Click" onclick="check_input()">
+      </p>
+  </form>
+  </section>
+  <hr>
+  <footer>
+    <?php include "footer.php";?>
+  </footer>
+</body>
+</html>
