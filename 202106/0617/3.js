803967d6700bec095c45ee673b45afd5a951ac05
leodin0717 <dkekatm55@gmail.com>
2021-06-18 00:46:59 +0900

    s

--- 202106/0617/3.js
+++ 202106/0617/3.js
@@ -5,11 +5,21 @@
 // }
 
 
+// const line = 5;
+// let star = '';
+// for(let i = 1; i<=line; i++){
+//   for(let j = 1; j<=(2*i)-1; j++){
+//     star += '*';
+//   }
+//   console.log(star);
+
 const line = 5;
 let star = '';
-for(let i = 1; i<=line; i++){
-  for(let j = 1; j<=(2*i)-1; j++){
-    star += '*';
-  }
-  console.log(star);
+for(let i = 1 ; i<=line; i++){
+  for(let j = 1; j<=line-i; j++)
+  
+  star += ' ';
+  for(let k = 1; k<=2*i-1; k++)
+  star += '*';
 }
+console.log(star);
